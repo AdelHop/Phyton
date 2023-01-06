a03be489bd07069ed806c316e247b42d3901dbfe
Adelina <77401712+AdelHop@users.noreply.github.com>
2023-01-06 23:10:40 +0100

    d4

--- 1d_Band_Name_Generator.py
+++ 1d_Band_Name_Generator.py
@@ -1,18 +1,14 @@
-
-#print("Hello " + (s=(input("What is your name?"))))
-#print (len(s))
-
-# Zad. 2 policzyć ilośc liter wy wypisywanym przez użytkownika słowie
 name = input("What is your name? ")
-print(len(name))
 
-# lub
+print ("Hello " + name )
 
-print( len( input("What is your name? ")))
+# Zad. 2 policzyć ilośc liter wypisywanym przez użytkownika słowie
+print("Your name consists of " + str(len(name)) + " characters.\n")
 
 print("Welcome to the Band Name Generator.")
 city = input("What's name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
+
 #print("What's name of the city you grew up in?")
 #city = input("")
 #print ("What's your pet's name?")
